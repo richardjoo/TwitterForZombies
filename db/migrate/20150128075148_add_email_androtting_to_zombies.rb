790fbc441e5b0f3e49863b66dc9c5d1a1e202e68
Richard Joo <richardjoo@gmail.com>
2015-01-28 00:30:22 -0800

    files updated

---
+++ db/migrate/20150128075148_add_email_androtting_to_zombies.rb
@@ -0,0 +1,6 @@
+class AddEmailAndrottingToZombies < ActiveRecord::Migration
+  def change
+    add_column :zombies, :email, :string
+    add_column :zombies, :rotting, :boolean, default: false
+  end
+end
